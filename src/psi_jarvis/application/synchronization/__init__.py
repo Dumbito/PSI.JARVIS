d0e86aa67eecc6f9320f866eaeab86b37d1db862
podewerr <109985018+Dumbito@users.noreply.github.com>
2026-09-06 23:43:18 -0500

    feat: export external synchronization orchestration

--- src/psi_jarvis/application/synchronization/__init__.py
+++ src/psi_jarvis/application/synchronization/__init__.py
@@ -1,12 +1,16 @@
 from psi_jarvis.application.synchronization.contracts import (
+    ExternalSynchronizationResult,
     MetadataChange,
     SyncConflict,
     SynchronizationResult,
 )
+from psi_jarvis.application.synchronization.external import ExternalBibliographicSynchronizationService
 from psi_jarvis.application.synchronization.service import BibliographicSynchronizationService
 
 __all__ = [
     "BibliographicSynchronizationService",
+    "ExternalBibliographicSynchronizationService",
+    "ExternalSynchronizationResult",
     "MetadataChange",
     "SyncConflict",
     "SynchronizationResult",
