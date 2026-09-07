c8d7692c920635224c946f8c9e8c5da9948fc37c
podewerr <109985018+Dumbito@users.noreply.github.com>
2026-09-06 23:34:17 -0500

    feat: add external synchronization contracts

---
+++ src/psi_jarvis/application/synchronization/__init__.py
@@ -0,0 +1,13 @@
+from psi_jarvis.application.synchronization.contracts import (
+    MetadataChange,
+    SyncConflict,
+    SynchronizationResult,
+)
+from psi_jarvis.application.synchronization.service import BibliographicSynchronizationService
+
+__all__ = [
+    "BibliographicSynchronizationService",
+    "MetadataChange",
+    "SyncConflict",
+    "SynchronizationResult",
+]
